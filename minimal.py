e8f99382001d8847e239fd09339374b0f44f4ce7
Owen Herrera <oherrera@ucvts.tec.nj.us>
2019-12-16 12:29:59 -0500

    new stuff

--- minimal.py
+++ minimal.py
@@ -3,7 +3,8 @@ import pygame
 
 # Initialize pygame so it runs in the background and manages things
 pygame.init()
-
+font = pygame.font.SysFont("Garamond", 100)
+text = font.render("Rachel", True, aqua)
 # Create a display. Size must be a tuple, which is why it's in parentheses
 screen = pygame.display.set_mode( (400, 300) )
 
@@ -11,7 +12,11 @@ screen = pygame.display.set_mode( (400, 300) )
 while True:
     # do something for each event in the event queue (list of things that happen)
     for event in pygame.event.get():
+        print(event)
         # Check to see if the current event is a QUIT event
         if event.type == pygame.QUIT:
             # If so, exit the program
             sys.exit()
+screen.blit(text, (0,0))            
+screen.fill(red)
+pygame.display.flip()
